a23c0631ef44e8b21f27984326ec864a2d5268ae
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-08-26 13:22:00 +0000

    deleted note.g.dart

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,9 +1,32 @@
 import 'package:drift/drift.dart';
+import 'dart:io';
 
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+part 'note.g.dart';
+
+@DataClassName('Note')
 class Note extends Table {
-  int id;
-  String title;
-  String details;
+  ColumnBuilder<int> get id => integer().autoIncrement();
+  ColumnBuilder<String> get title => text().withLength(min: 6, max: 32);
+  ColumnBuilder<String> get content => text().named('body');
+}
 
-  Note(this.id, this.title, this.details);
+@DriftDatabase(tables: [Note])
+class NoteDatabase extends _$NoteDatabase {
+
+  NoteDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+
+  LazyDatabase _openConnection() {
+    return LazyDatabase(() async {
+      final dbFolder = await getApplicationDocumentsDirectory();
+      final file = File(p.join(dbFolder.path, 'db.sqlite'));
+      return NativeDatabase.createInBackground(file);
+    });
+  }
 }
