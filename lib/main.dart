1a40f331682f46408d3a5a7ec9fafc06e2f2effa
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-08-26 11:24:14 +0000

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:scrivania/app/app.dart';
+import 'package:scrivania/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const Scrivania());
 }
 
