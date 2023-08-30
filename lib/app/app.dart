b46d5abd683c2bcba26a7e5cf24d4216a6cf5e85
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-08-30 18:19:04 +0000

    added textTheme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:scrivania/app/presentation/app_settings.dart';
 import 'package:scrivania/app/presentation/home.dart';
 
@@ -12,9 +13,13 @@ class Scrivania extends StatelessWidget {
       title: 'Scrivania',
       theme: ThemeData.light(
         useMaterial3: true
+      ).copyWith(
+          textTheme: GoogleFonts.spaceGroteskTextTheme()
       ),
       darkTheme: ThemeData.dark(
         useMaterial3: true,
+      ).copyWith(
+        textTheme: GoogleFonts.spaceGroteskTextTheme()
       ),
       themeMode: ThemeMode.system,
       home: const Home(),
